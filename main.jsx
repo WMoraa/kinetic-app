1edccc510badacb6f678a258ff8a2af98047e24e
WMoraa <66967843+WMoraa@users.noreply.github.com>
2026-04-22 10:31:37 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Kinetic from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Kinetic />
-  </React.StrictMode>,
-)
